f4ca3eebe193d9d0a1d3a521572ee587004fef43
Ana Zeneli <anazeneli@Anas-MacBook-Pro.local>
2018-01-03 13:13:55 -0500

    Added dom-events functionality

--- dom-events.js
+++ dom-events.js
@@ -15,18 +15,49 @@ const astrosEl = document.getElementById("astros")
 //   astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
 // }
 
- // create a function that alerts "these are all the astronauts!" when the title element is clicked.
+ // create a function that alerts "these are all the astronauts!"
+ // when the title element is clicked.
+ var title = document.getElementsByClassName("title")[0]
+ title.onclick = () => {
+   alert("these are all the astronausts!")
+ }
 
- // log to the console the client's x coordinate of their mouse as they move it around the window.
+// HTML element
+// log to the console the client's x coordinate of their
+// mouse as they move it around the window.
+const body = document.getElementsByTagName("body")[0]
+
+// when event listener fires of, creates event
+body.onmouseover = (event) => {
+  console.log(event.screenX)
+}
 
 // make an element that displays the most recent key pressed in the DOM
+body.onkeypress = (event) => {
+  console.log(event.key)
+}
 
-// create a text input element. When a user clicks on the text field to input, log the input element to console.
+// create a text input element. When a user clicks on the text field to input,
+// log the input element to console.
+
+
+
 
 // when a user clicks away from the input, log ("bye") to the console.
 
 // wrap the input element with a form element.
 
 // when the form is submitted, render the client's inputted text to the DOM
+const form = document.getElementById("iform")
+const textInput = document.getElementById("textInput")
+const answer = document.getElementById("textInput")
+
+form.onSubmit = (event) => {
+  // prevents submit from refreshing the page
+  event.preventDefault()
+  console.log(textInput.value)
+  answer.innerText = textInput.value
+}
+
 
 // HINT: look into event.preventDefault()
